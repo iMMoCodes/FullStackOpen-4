9844a8a150578ea7f62f4df4dd1b854bcc79f0ea
iMMoCodes <matti-makela@hotmail.com>
2022-01-17 02:36:35 +0200

    Tehtävä 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,18 @@
+// Dummy
 const dummy = (blogs) => {
   return 1
 }
 
+// Total Likes
+const totalLikes = (blogs) => {
+  const likes = blogs.map((blog) => {
+    return blog.likes
+  })
+
+  return likes.reduce((acc, curr) => acc + curr, 0)
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 }
