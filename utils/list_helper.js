63107825e6f4343a6e81e3160e0dc412ca89a458
iMMoCodes <matti-makela@hotmail.com>
2022-01-17 03:14:58 +0200

    Tehtävä 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -12,7 +12,20 @@ const totalLikes = (blogs) => {
   return likes.reduce((acc, curr) => acc + curr, 0)
 }
 
+// Highest Likes
+const favoriteBlog = (blogs) => {
+  const likes = blogs.map((blog) => {
+    return blog.likes
+  })
+  const highestAmountOfLikes = Math.max(...likes)
+  const blogWithHighestLikes = blogs.filter(
+    (el) => el.likes === highestAmountOfLikes
+  )
+  return blogWithHighestLikes[0]
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
