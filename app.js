ddb39438efc0b82e4d3200dbd8bcd6a6efe8c3ab
iMMoCodes <matti-makela@hotmail.com>
2022-01-21 02:02:01 +0200

    Tehtävä 4.20

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+const tokenExtractor = require('./middlewares/tokenExtractor')
 
 mongoose
   .connect(config.MONGODB_URI)
@@ -22,6 +23,7 @@ mongoose
 
 app.use(cors())
 app.use(express.json())
+app.use(tokenExtractor)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,14 +3,6 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
-const getTokenFrom = (request) => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
-
 blogsRouter.get('/', async (request, response) => {
   try {
     const blogs = await Blog.find({}).populate('user', {
@@ -27,10 +19,13 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   try {
     const body = request.body
-    const token = getTokenFrom(request)
-    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!request.token) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
 
-    if (!token || !decodedToken.id) {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+
+    if (!decodedToken.id) {
       return response.status(401).json({ error: 'token missing or invalid' })
     }
 
